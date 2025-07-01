392fc0f6ba8e77c38afad1b4605d6abeba7439da
brenda-ncc <37358704@ifts24.edu.ar>
2025-06-30 22:20:16 -0300

    se comenta html home geoloca

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,9 @@ import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
 
-  {
+   {
     path: 'login', loadComponent: () => import('./pages/login/login.page').then(m => m.LoginPage)
-  },
+  }, 
 
   {
     path: 'home', loadComponent: () => import('./pages/home/home.page').then(m => m.HomePage), //canActivate: [authGuard],
